17b472cfd349142a220c8cfd8f0176805ddf89ea
蓝田_Loto <shorfng@126.com>
2021-09-02 20:14:05 +0800

    MiniTomcat-V1

---
+++ 11.1_MiniTomcat/src/main/java/com/loto/tomcat/run/BootstrapV1.java
@@ -0,0 +1,58 @@
+package com.loto.tomcat.run;
+
+import com.loto.tomcat.util.HttpProtocolUtil;
+import lombok.Data;
+
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Author：蓝田_Loto
+ * <p>Date：2021-09-02 19:36</p>
+ * <p>PageName：BootstrapV1.java</p>
+ * <p>Function：启动类</p>
+ */
+
+@Data
+public class BootstrapV1 {
+    /**
+     * 定义 socket 监听的端口号
+     */
+    private int port = 8080;
+
+    /**
+     * Minicat 的程序启动入口
+     */
+    public static void main(String[] args) {
+        BootstrapV1 bootstrap = new BootstrapV1();
+
+        try {
+            // 启动 Minicat
+            bootstrap.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Minicat 1.0版本：浏览器请求 http://localhost:8080，返回一个固定的字符串到页面 "Hello Minicat!"
+    /**
+     * Minicat 启动需要初始化展开的一些操作
+     */
+    public void start() throws Exception {
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("=====>>> Minicat start on port：" + port);
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+
+            // 获取输出流
+            OutputStream outputStream = socket.getOutputStream();
+
+            String data = "Hello Minicat!";
+            String responseText = HttpProtocolUtil.getHttpHeader200(data.getBytes().length) + data;
+            outputStream.write(responseText.getBytes());
+            socket.close();
+        }
+    }
+}
---
+++ 11.1_MiniTomcat/src/main/java/com/loto/tomcat/util/HttpProtocolUtil.java
@@ -0,0 +1,28 @@
+package com.loto.tomcat.util;
+
+/**
+ * http 协议工具类，主要是提供响应头信息，这里只提供200和404的情况
+ */
+public class HttpProtocolUtil {
+    /**
+     * 为响应码 200 提供请求头信息
+     */
+    public static String getHttpHeader200(long contentLength) {
+        return "HTTP/1.1 200 OK \n" +
+                "Content-Type: text/html \n" +
+                "Content-Length: " + contentLength + " \n" +
+                "\r\n";
+    }
+
+    /**
+     * 为响应码 404 提供请求头信息(此处也包含了数据内容)
+     */
+    public static String getHttpHeader404() {
+        String str404 = "<h1>404 not found</h1>";
+
+        return "HTTP/1.1 404 NOT Found \n" +
+                "Content-Type: text/html \n" +
+                "Content-Length: " + str404.getBytes().length + " \n" +
+                "\r\n" + str404;
+    }
+}
